ed513cdf4ec012fd60f2cf4c878dc44878c6a7f5
Mauricio Parra Casado <mauryparra@gmail.com>
2017-02-01 10:31:03 -0300

    Modelo/Migración  CategoriaGasto

---
+++ database/migrations/2017_01_31_232103_create_categorias_gastos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriasGastosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias_gastos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorias_gastos');
+    }
+}
