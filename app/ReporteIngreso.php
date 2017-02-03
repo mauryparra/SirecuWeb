e362c977253876db6fa20e001bcdc04321c113a4
Mauricio Parra Casado <mauryparra@gmail.com>
2017-02-02 23:14:57 -0300

    Agregar timestamps a ReporteIngreso

--- app/ReporteIngreso.php
+++ app/ReporteIngreso.php
@@ -18,7 +18,7 @@ class ReporteIngreso extends Model
      *
      * @var bool
      */
-    public $timestamps = false;
+    public $timestamps = true;
 
     /**
      * The attributes that are mass assignable.
