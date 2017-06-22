8deafe4b265a560dc83dd5e7e52847d854d4b165
Mauricio Parra Casado <mauryparra@gmail.com>
2017-06-21 23:45:00 -0300

    Filtro y Grafico para Ingreso/Egreso

--- app/Http/Controllers/GraficoController.php
+++ app/Http/Controllers/GraficoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\ReporteTrimestral;
+use App\Seccional;
+use App\Trimestre;
 use Charts;
 
 class GraficoController extends Controller
@@ -26,29 +28,59 @@ class GraficoController extends Controller
      */
     public function index(Request $request)
     {
-        if ($request->exists('grafico') && ($request->exists('seccional')) && ($request->exists('año')))
+        if ($request->exists(['grafico', 'seccional', 'trimestreDesde', 'añoDesde', 'trimestreHasta', 'añoHasta']))
         {
-            $data = ReporteTrimestral::where('seccional_id', $request->input('seccional'))
-                ->where('año', $request->input('año'))
-                ->with('trimestre', 'seccional', 'ingreso', 'egreso')
-                ->get();
+            if ($request->input('grafico') == "ingreso/egreso")
+            {
+                $data = ReporteTrimestral::where('seccional_id', $request->input('seccional'))
+                    ->where('trimestre_id', '>=', $request->input('trimestreDesde'))
+                    ->where('año', '>=', $request->input('añoDesde'))
+                    ->where('trimestre_id', '<=', $request->input('trimestreHasta'))
+                    ->where('año', '<=', $request->input('añoHasta'))
+                    ->with('trimestre', 'seccional', 'ingreso', 'egreso')
+                    ->get();
 
-            $chart = Charts::multi('areaspline', 'highcharts')
-                ->title('Ingresos/Egresos')
-                ->colors(['#4572a7', '#aa4643'])
-                ->labels($data->map(function ($item) {
-                    return $item->trimestre->nombre . ' ' .
-                        $item->año;
-                    }))
-                ->plotBandsFrom(0)
-                ->plotBandsTo(0)
-                ->dataset('Ingresos', $data->pluck('ingresos'))
-                ->dataset('Egresos',  $data->pluck('egresos'));
+                $chart = Charts::multi('areaspline', 'highcharts')
+                    ->title('Ingresos/Egresos')
+                    ->colors(['#4572a7', '#aa4643'])
+                    ->labels($data->map(function ($item) {
+                        return $item->trimestre->nombre . ' ' .
+                            $item->año;
+                        }))
+                    ->plotBandsFrom(0)
+                    ->plotBandsTo(0)
+                    ->dataset('Ingresos', $data->pluck('ingresos'))
+                    ->dataset('Egresos',  $data->pluck('egresos'));
 
-            return view('graficos.index', compact('chart'));
+            }
+            elseif ($request->input('grafico') == "ingresos") {
+                # code...
+            }
+            elseif ($request->input('grafico') == "gastos") {
+                # code...
+            }
+            elseif ($request->input('grafico') == "saldos") {
+                # code...
+            }
+            else {
+                return view('graficos.index', [
+                    'seccionales' => Seccional::orderBy('id', 'desc')->get(),
+                    'trimestres' => Trimestre::all()
+                ]);
+            }
+
+            return view('graficos.index', [
+                'chart' => $chart,
+                'seccionales' => Seccional::orderBy('id', 'desc')->get(),
+                'trimestres' => Trimestre::all()
+            ]);
+
         }
         else {
-            return view('graficos.index');
+            return view('graficos.index', [
+                'seccionales' => Seccional::orderBy('id', 'desc')->get(),
+                'trimestres' => Trimestre::all()
+            ]);
         }
 
 
--- resources/views/bloques/filtro-grafico.blade.php
+++ resources/views/bloques/filtro-grafico.blade.php
@@ -2,28 +2,63 @@
     <div class="panel-heading">Buscar</div>
 
     <div class="panel-body text-center">
-        <form class="form-inline" role="form" method="GET">
-
-            <div class="form-group">
-                <label for="grafico">Tipo de Gráfico</label>
-                <select class="form-control" name="grafico" required>
-                    <option value="ingreso/egreso" {{ Request::input('grafico') === "ingreso/egreso" ? "selected" : "" }}>Ingresos/Egresos</option>
-                    <option value="ingresos" {{ Request::input('grafico') === "ingresos" ? "selected" : "" }}>Fuentes de Ingresos</option>
-                    <option value="gastos" {{ Request::input('grafico') === "gastos" ? "selected" : "" }}>Categorías de Gasto</option>
-                    <option value="saldos" {{ Request::input('grafico') === "saldos" ? "selected" : "" }}>Saldos Finales por Trimestre</option>
-                </select>
+        <form class="form" role="form" method="GET">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label for="grafico">Tipo de Gráfico</label>
+                        <select class="form-control" name="grafico" required>
+                            <option value="ingreso/egreso" {{ Request::input('grafico') == "ingreso/egreso" ? "selected" : "" }}>Ingresos/Egresos</option>
+                            <option value="ingresos" {{ Request::input('grafico') === "ingresos" ? "selected" : "" }}>Fuentes de Ingresos</option>
+                            <option value="gastos" {{ Request::input('grafico') === "gastos" ? "selected" : "" }}>Categorías de Gasto</option>
+                            <option value="saldos" {{ Request::input('grafico') === "saldos" ? "selected" : "" }}>Saldos Finales por Trimestre</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label for="seccional">Seccional</label>
+                        <select class="form-control" name="seccional" required>
+                            @foreach ($seccionales as $seccional)
+                                <option value="{{ $seccional->id }}" {{ Request::input('seccional') == $seccional->id ? "selected" : "" }}>{{ $seccional->nombre }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label for="trimestreDesde">Desde</label>
+                        <select class="form-control" name="trimestreDesde" required>
+                            @foreach ($trimestres as $trimestre)
+                                <option value="{{ $trimestre->id }}" {{ Request::input('trimestreDesde') == $trimestre->id ? "selected" : "" }}>{{ $trimestre->nombre }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="seccional">Seccional</label>
-                <select class="form-control" name="seccional" required>
-                    @foreach (App\Seccional::orderBy('id', 'desc')->get() as $seccional)
-                        <option value="{{ $seccional->id }}" {{ Request::input('seccional') === $seccional->id ? "selected" : "" }}>{{ $seccional->nombre }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="año">Año</label>
-                <input type="numeric" class="form-control" name="año" value="{{ Request::input('año') }}" placeholder="2017" required>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label for="añoDesde">Año</label>
+                        <input type="numeric" class="form-control" name="añoDesde" value="{{ Request::input('añoDesde') }}" placeholder="2017" required>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label for="trimestreHasta">Hasta</label>
+                        <select class="form-control" name="trimestreHasta" required>
+                            @foreach ($trimestres as $trimestre)
+                                <option value="{{ $trimestre->id }}" {{ Request::input('trimestreHasta') == $trimestre->id ? "selected" : "" }}>{{ $trimestre->nombre }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label for="añoHasta">Año</label>
+                        <input type="numeric" class="form-control" name="añoHasta" value="{{ Request::input('añoHasta') }}" placeholder="2017" required>
+                    </div>
+                </div>
             </div>
             <button type="submit" class="btn btn-default">Graficar</button>
         </form>
