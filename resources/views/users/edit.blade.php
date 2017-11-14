320a5b4068f6c4ae13d6bc49bea7c4cd7a101567
Mauricio Parra Casado <mauryparra@gmail.com>
2017-11-14 01:36:02 -0300

    Fix
    * Se corrigio campo de nombre en caso de error de validación de formulario
    + Se agrego la clase active para los páginas actuales en el menu de navegación

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -15,7 +15,7 @@
                             <label for="name" class="col-md-4 control-label">@lang('Name')</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('email', $userEdit->name) }}" required autofocus>
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name', $userEdit->name) }}" required autofocus>
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
